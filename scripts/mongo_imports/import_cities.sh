6ebdc5018b074369ec630b18485cbeb806786f10
agustinlinari <agustinlinari@gmail.com>
2017-08-22 20:40:37 -0300

    base en la nube mongolab

--- scripts/mongo_imports/import_cities.sh
+++ scripts/mongo_imports/import_cities.sh
@@ -1,1 +1,2 @@
-mongoimport --db dbtp0 --collection cities --file city.list.json --jsonArray
+#mongoimport --db dbtp0 --collection cities --file city.list.json --jsonArray
+mongoimport -h ds023463.mlab.com:23463 -d dbtp0 -c cities -u tdpuser -p 12345 --file city.list.json --jsonArray
