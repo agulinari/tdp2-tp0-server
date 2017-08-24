a595f1af85e8a5b4020f7757ad506eefab186beb
agustinlinari <agustinlinari@gmail.com>
2017-08-24 19:52:24 -0300

    fix url base

--- src/model/db.js
+++ src/model/db.js
@@ -1,5 +1,5 @@
 exports = mongoose = require('mongoose');
-var connectionUri = "mongodb://127.0.0.1/dbtp0";
-//var connectionUri = "mongodb://tdpuser:12345@ds023463.mlab.com:23463/dbtp0";
+//var connectionUri = "mongodb://127.0.0.1/dbtp0";
+var connectionUri = "mongodb://tdpuser:12345@ds023463.mlab.com:23463/dbtp0";
 mongoose.connect(connectionUri,{ useMongoClient: true });
 exports = Schema = mongoose.Schema;
